a09f9e3dbeb615098a30f762cda3a777b585d8dd
Romero <rrcb@cin.ufpe.br>
2021-10-02 01:39:46 -0300

    feat: Create function to send message
    Already tested, basic functionality working well

---
+++ main/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+class ChatUser(models.Model):
+    username = models.CharField(max_length=12, unique=True)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=20)
+    contacts = models.ManyToManyField('self', symmetrical=False)
+    messages = models.ManyToManyField('self', through='Message')
+
+    @classmethod
+    def create(cls, username, email, password):
+        user = cls(username=username, email=email, password=password)
+        return user
+
+    def sendMessage(self, user, text):
+        message = Message.create(self, user, text)
+        message.save()
+        print("message sent successfully")
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(ChatUser, on_delete=models.SET_NULL, related_name='sender', null=True)
+    receiver = models.ForeignKey(ChatUser, on_delete=models.SET_NULL, related_name='receiver', null=True)
+    text = models.TextField()
+    is_read = models.BooleanField(default=False)
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    @classmethod
+    def create(cls, sender, receiver, text):
+        message = cls(sender=sender, receiver=receiver, text=text)
+        return message
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        ordering = ('sent_at',)
+
+    
---
+++ main/tests.py
@@ -0,0 +1,5 @@
+from django.test import TestCase
+from main.models import ChatUser, Message
+
+        
+        
