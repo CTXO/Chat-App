43e6fe4947fc282cb165063a2ceea62c73ea5d18
Romero <rrcb@cin.ufpe.br>
2022-06-07 01:38:30 -0300

    Add react router

--- chat_frontend/src/App.js
+++ chat_frontend/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import React  from 'react'
+import { Button } from "@mui/material";
+import { HashRouter as Router, Route, Routes, Navigate } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Set up react
-      </h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Navigate replace to='/login'/>}/>
+        <Route path='/login' element={<h1>Login page</h1>}/>
+        <Route path="/dashboard" element={<h1>Dashboard page</h1>}>
+          Dashboard page
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
